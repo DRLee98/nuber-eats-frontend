f52e9b2a5499d6d87083f7b54cd715c9cbd78700
DRLee98 <64233118+DRLee98@users.noreply.github.com>
2020-12-26 14:53:40 +0900

    Restaurant

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -7,7 +7,11 @@ import {
 } from "react-router-dom";
 import { Header } from "../components/header";
 import { useMe } from "../hooks/useMe";
+import { NotFound } from "../pages/404";
+import { Category } from "../pages/client/category";
+import { Restaurant } from "../pages/client/restaurant";
 import { Restaurants } from "../pages/client/restaurants";
+import { Search } from "../pages/client/search";
 import { ConfirmEmail } from "../pages/user/confirm-email";
 import { EditProfile } from "../pages/user/edit-profile";
 
@@ -15,11 +19,20 @@ const ClientRoutes = [
   <Route key={1} path="/" exact>
     <Restaurants />
   </Route>,
-  <Route key={2} path="/confirm" exact>
+  <Route key={2} path="/confirm">
     <ConfirmEmail />
   </Route>,
-  <Route key={3} path="/edit-profile" exact>
+  <Route key={3} path="/edit-profile">
     <EditProfile />
+  </Route>,
+  <Route key={4} path="/search">
+    <Search />
+  </Route>,
+  <Route key={5} path="/category/:slug">
+    <Category />
+  </Route>,
+  <Route key={6} path="/restaurants/:id">
+    <Restaurant />
   </Route>,
 ];
 
@@ -37,7 +50,9 @@ export const LoggedInRouter = () => {
       <Header />
       <Switch>
         {data.me.role === "Client" && ClientRoutes}
-        <Redirect to="/" />
+        <Route>
+          <NotFound />
+        </Route>
       </Switch>
     </Router>
   );
